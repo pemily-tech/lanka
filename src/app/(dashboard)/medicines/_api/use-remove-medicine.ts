daad020d0b32d80314679c82e479ceed32d64908
vakadu <vakadu10@gmail.com>
2025-04-21 20:44:59 +0530

    feat(ui): removed medine added

---
+++ src/app/(dashboard)/medicines/_api/use-remove-medicine.ts
@@ -0,0 +1,43 @@
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
+import { HttpService } from '../../../../services/http-service';
+import { queryClient } from '../../../../services/providers';
+import { type IApiResponse } from '../../../../types/common';
+import { type IMedicine } from '../../../../types/prescription';
+
+import { env } from '@/env.mjs';
+
+const removeMedicine = async (payload: { id: string }) => {
+	const { id } = payload;
+	try {
+		const { data } = await HttpService.patch<
+			IApiResponse<{ medicine: IMedicine }>
+		>(`${env.NEXT_PUBLIC_BASE_PATH}/medicine/remove/${id}`);
+		return data;
+	} catch (err) {
+		throw new Error('Network Error');
+	}
+};
+
+export const useRemoveMedicine = () => {
+	return useMutation({
+		mutationFn: (payload: { id: string }) => removeMedicine(payload),
+		onSuccess: (data) => {
+			if (data?.status === 'SUCCESS') {
+				queryClient.invalidateQueries({
+					queryKey: [
+						'medicine/list',
+						{ page: 0, count: 1, limit: 15 },
+					],
+				});
+				toast.success('Removed Successfully!');
+			} else {
+				toast.error('Something went wrong. Please try again');
+			}
+		},
+		onError: (err) => {
+			toast.error(err.message);
+		},
+	});
+};
--- src/app/(dashboard)/medicines/list/_ui/columns.tsx
+++ src/app/(dashboard)/medicines/list/_ui/columns.tsx
@@ -1,22 +1,36 @@
 import { type ColumnDef } from '@tanstack/react-table';
+import { Edit2, LogOutIcon, Trash2 } from 'lucide-react';
 import Link from 'next/link';
 
 import { Routes } from '../../../../../helpers/routes';
 import { cn } from '../../../../../helpers/utils';
 import { type IMedicine } from '../../../../../types/prescription';
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+	Button,
+} from '../../../../../ui/shared';
+import { useRemoveMedicine } from '../../_api/use-remove-medicine';
 
 export function useColumns(): ColumnDef<IMedicine>[] {
+	const { mutate: removeMedicine } = useRemoveMedicine();
+
+	const handelRemove = (id: string) => {
+		removeMedicine({ id });
+	};
+
 	return [
 		{
 			accessorKey: 'medicineId',
 			header: 'ID',
-			cell: ({ row }) => (
-				<Link
-					href={`${Routes.MEDICINES_UPDATE}/${row.original.medicineId}`}
-				>
-					{row.original.medicineId}
-				</Link>
-			),
+			cell: ({ row }) => <div>{row.original.medicineId}</div>,
 		},
 		{
 			accessorKey: 'name',
@@ -44,6 +58,52 @@ export function useColumns(): ColumnDef<IMedicine>[] {
 					)}
 				>
 					{row.original.active ? 'Active' : 'InActive'}
+				</div>
+			),
+		},
+		{
+			id: 'buttons',
+			header: '',
+			cell: ({ row }) => (
+				<div className="flex items-center gap-12">
+					<Link
+						href={`${Routes.MEDICINES_UPDATE}/${row.original.medicineId}`}
+						className="flex size-24 items-center justify-center"
+					>
+						<Button size="icon" variant="ghost">
+							<Edit2 className="size-18" />
+						</Button>
+					</Link>
+					<AlertDialog>
+						<AlertDialogTrigger asChild>
+							<Button size="icon" variant="ghost">
+								<Trash2 className="size-18 text-destructive" />
+							</Button>
+						</AlertDialogTrigger>
+						<AlertDialogContent className="gap-24">
+							<AlertDialogHeader>
+								<AlertDialogTitle className="text-24">
+									Delete
+								</AlertDialogTitle>
+								<AlertDialogDescription>
+									Are you sure you want to delete?
+								</AlertDialogDescription>
+							</AlertDialogHeader>
+							<AlertDialogFooter className="!pt-32">
+								<AlertDialogAction
+									onClick={() =>
+										handelRemove(row.original.medicineId)
+									}
+									className="px-24"
+								>
+									Confirm
+								</AlertDialogAction>
+								<AlertDialogCancel>
+									<span className="text-14">Cancel</span>
+								</AlertDialogCancel>
+							</AlertDialogFooter>
+						</AlertDialogContent>
+					</AlertDialog>
 				</div>
 			),
 		},
