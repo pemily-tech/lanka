061bd836304212198d260ce4c91f6e12eccd55e3
pemilytech <pemily.tech@gmail.com>
2025-05-17 21:46:29 +0530

    feat(ui): added doctor collapse

--- src/app/(dashboard)/user-profile/_ui/tabs/index.tsx
+++ src/app/(dashboard)/user-profile/_ui/tabs/index.tsx
@@ -14,6 +14,7 @@ import {
 import AddressForm from './address';
 import BusinessForm from './business-details';
 import Contact from './contact';
+import Doctors from './doctors';
 import PersonalDetailsForm from './personal-details';
 
 export default function UserTabs() {
@@ -52,6 +53,12 @@ export default function UserTabs() {
 			label: 'Business Details',
 			value: 'business',
 			component: <BusinessForm />,
+			role: [Roles.Clinic],
+		},
+		{
+			label: 'Doctors',
+			value: 'doctors',
+			component: <Doctors />,
 			role: [Roles.Clinic],
 		},
 		{
