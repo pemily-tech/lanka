f8f74567abcb54b9f5182a525d6985de49ba9fe1
pemilytech <pemily.tech@gmail.com>
2025-08-16 16:38:20 +0530

    feat(fix): removed dummy favicon

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -64,7 +64,7 @@ export const AppSidebar = () => {
 					</SidebarMenuItem>
 				</SidebarMenu>
 			</SidebarHeader>
-			<SidebarContent>
+			<SidebarContent className="scrollbar-custom">
 				<SidebarGroup>
 					<Menu navMenu={filteredNavMenu} role={role} />
 				</SidebarGroup>
